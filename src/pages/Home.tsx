a79c361c90cb5978ab8dafed1f25607c2d8c621f
Ahmet Burak KIR <abkburak1@gmail.com>
2026-02-16 11:22:36 +0300

    First Commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Web App</h1>
+      <p>This is the web version of our application.</p>
+    </div>
+  );
+};
+
+export default Home;
